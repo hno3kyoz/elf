3a6869746f4461c87cd820b45a76b4ceb32e2dcd
Pin Xie <pin.xie@ge.com>
2017-04-17 16:38:40 +0800

    add stderr support

--- browser.py
+++ browser.py
@@ -7,16 +7,18 @@ async def handle(request):
     if name[0] != "/":
         name = "/" + name
 
-    cmd = "ls -l " + name
+    cmd = "ls -la " + name
 
     p = await asyncio.create_subprocess_shell(cmd,
                                               stdout=asyncio.subprocess.PIPE,
                                               stderr=asyncio.subprocess.PIPE)
     returncode = await p.wait()
     out = (await p.stdout.read()).decode("utf-8")
-    print(out)
+    err = (await p.stderr.read()).decode("utf-8")
+    text = out + err
+    print(text)
 
-    return web.Response(text=out)
+    return web.Response(text=text)
 
 app = web.Application()
 app.router.add_get('/', handle)
